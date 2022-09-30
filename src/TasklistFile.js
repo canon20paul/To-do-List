9cbda81f918201873b1f2739b61ffae6baad5029
canon20paul <canon20paul@gmail.com>
2022-09-30 17:51:06 +0300

    Deletion, Editing , Index Renddering and local storage Configured

--- src/TasklistFile.js
+++ src/TasklistFile.js
@@ -1,33 +1,28 @@
+import { update } from "lodash";
 
 
 export  class TaskList {
      constructor (){
-        
-        // if(!this.taskArray){
-        //  this.taskArray=[];}
-        //  else{
-        //     this.taskArray===this.taskArray;
-        //  }
-         this.taskArray = JSON.parse(localStorage.getItem('coward')) || [];
-        
-    }
+        this.taskArray = JSON.parse(localStorage.getItem('coward')) || [];
+        }
+
     static addTask(desc){
         let index = this.taskArray.length ;
         let task = {
             description: desc,
             completed: false,
-            index: 1
+            index: index
         }
-        
         this.taskArray.push(task);
-        localStorage.setItem('coward', JSON.stringify(this.tas));
+        
+        localStorage.setItem('coward', JSON.stringify(this.taskArray));
     }
 
-    static Render(){
-        const arr = JSON.parse(localStorage.getItem('coward')) || [];
-        return arr;
-    }
-
-    
-
+    static delete (index) {
+        this.taskArray.splice(index, 1);
+        for (let i = 0; i < this.taskArray.length; i = 1 + i) {
+            this.taskArray[i].index = i ;
+        }
+        localStorage.setItem('coward', JSON.stringify(this.taskArray));
+  }
 }
